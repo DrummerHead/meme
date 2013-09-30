bc377a2671379cb6c48f69d4815ee897f0be8807
DrummerHead <drummerhead@gmail.com>
2013-09-30 13:08:25 -0300

    Change app structure, share data between controllers with factory

--- js/gedrag.js
+++ js/gedrag.js
@@ -1,18 +1,35 @@
 'use strict';
 
-angular.module('ngMeme', [], function($routeProvider, $locationProvider){
-  $routeProvider.when('/', {
-    templateUrl: 'root.html',
-    controller: rootController
-  });
-  $routeProvider.when('/:img', {
-    templateUrl: 'image.html',
-    controller: imageController
-  });
-  $routeProvider.otherwise({ // Quite useless, I'll leave it for reference
-    redirectTo: '/'
-  });
-});
+var ngMeme = angular.module('ngMeme', [])
+
+ngMeme.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider.when('/', {
+      templateUrl: 'root.html',
+      controller: rootController
+    });
+    $routeProvider.when('/:img', {
+      templateUrl: 'image.html',
+      controller: imageController
+    });
+    $routeProvider.otherwise({ // Quite useless, I'll leave it for reference
+      redirectTo: '/'
+    });
+  }]);
+
+ngMeme.factory('Toma', function(){
+  var memeData = {};
+
+  return {
+    getMemeData : function(){
+      console.log(memeData);
+      return memeData;
+    },
+    setMemeData : function(data){
+      memeData = data;
+      console.log(memeData);
+    }
+  }
+})
 
 var localhost = true;
 var docRoot = (localhost ? 'http://localhost:8080/#/' : 'http://mcdlr.com/lalalele')
@@ -22,22 +39,37 @@ function urlfy(input){
 }
 
 
-function mainController($scope, $route, $routeParams, $location){
+function mainController($scope, $route, $routeParams, $location, Toma){
   $scope.$route = $route;
   $scope.$location = $location;
   $scope.$routeParams = $routeParams;
+  Toma.getMemeData();
 }
 
-function rootController($scope, $route, $routeParams, $location){
+function rootController($scope, $route, $routeParams, $location, Toma){
   $scope.createLink = function(){
     $scope.lincoln = docRoot + 'img?url=' + encodeURIComponent($scope.imageUrl) + '&fl=' + encodeURIComponent($scope.firstLine) + '&sl=' + encodeURIComponent($scope.secondLine)
   };
+  var lasCosas = Toma.getMemeData();
+
+  $scope.imageUrl = lasCosas.imageUrl;
+  $scope.firstLine = lasCosas.firstLine;
+  $scope.secondLine = lasCosas.secondLine;
 }
 
-function imageController($scope, $route, $routeParams, $location){
+function imageController($scope, $route, $routeParams, $location, Toma){
+  //$scope.sicodelia = Toma.getCoso();
   $scope.imageUrl = $routeParams.url;
   $scope.firstLine = $routeParams.fl;
   $scope.secondLine = $routeParams.sl;
+
+  var dataMagica = {
+    'imageUrl' : $routeParams.url,
+    'firstLine' : $routeParams.fl,
+    'secondLine' : $routeParams.sl
+  };
+
+  Toma.setMemeData(dataMagica)
 }
 
 function Memize($scope){
