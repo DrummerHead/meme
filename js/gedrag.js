4893204952764b0b9942bb97c553abc58fa928ab
DrummerHead <drummerhead@gmail.com>
2013-09-30 11:55:44 -0300

    Add basic sharing of meme

--- js/gedrag.js
+++ js/gedrag.js
@@ -1,14 +1,25 @@
+'use strict';
+
 angular.module('ngMeme', [], function($routeProvider, $locationProvider){
   $routeProvider.when('/', {
     templateUrl: 'root.html',
     controller: rootController
   });
-  $routeProvider.when('/:imgPath', {
+  $routeProvider.when('/:img', {
     templateUrl: 'image.html',
     controller: imageController
   });
+  $routeProvider.otherwise({ // Quite useless, I'll leave it for reference
+    redirectTo: '/'
+  });
 });
 
+var localhost = true;
+var docRoot = (localhost ? 'http://localhost:8080/#/' : 'http://mcdlr.com/lalalele')
+
+function urlfy(input){
+  return input.replace(/ /g, '-');
+}
 
 
 function mainController($scope, $route, $routeParams, $location){
@@ -19,12 +30,14 @@ function mainController($scope, $route, $routeParams, $location){
 
 function rootController($scope, $route, $routeParams, $location){
   $scope.createLink = function(){
-    console.log('does');
-    console.log($scope.imageUrl + ' ' + $scope.firstLine + ' ' + $scope.secondLine);
+    $scope.lincoln = docRoot + 'img?url=' + encodeURIComponent($scope.imageUrl) + '&fl=' + encodeURIComponent($scope.firstLine) + '&sl=' + encodeURIComponent($scope.secondLine)
   };
 }
 
 function imageController($scope, $route, $routeParams, $location){
+  $scope.imageUrl = $routeParams.url;
+  $scope.firstLine = $routeParams.fl;
+  $scope.secondLine = $routeParams.sl;
 }
 
 function Memize($scope){
